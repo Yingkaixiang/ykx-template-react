e56bcd707ab5935351ba34302f8aea4ea91e10c8
Yingkaixiang <rubykaikai@gmail.com>
2018-11-21 14:12:57 +0800

    feat: Add react router with dynamic import

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,57 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
+import { createBrowserHistory } from "history";
+import { Switch, Route } from "react-router-dom";
+import Loadable from "react-loadable";
+import {
+  connectRouter,
+  routerMiddleware,
+  ConnectedRouter,
+} from "connected-react-router";
 
 import registerServiceWorker from "./registerServiceWorker";
 
-import App from "@/pages/App/";
+// import App from "@/pages/App/";
+import Loading from "@/pages/Loading";
+import NotFound from "@/pages/NotFound";
 
 import * as app from "@/pages/App/model/";
 
+const history = createBrowserHistory();
+
 const reducers = combineReducers({
   app: app.reducers,
+  router: connectRouter(history),
 });
 
 const sagaMiddlewares = createSagaMiddleware();
 
-const store = createStore(reducers, {}, applyMiddleware(sagaMiddlewares));
+const store = createStore(
+  reducers,
+  {},
+  compose(applyMiddleware(sagaMiddlewares, routerMiddleware(history))),
+);
 
 sagaMiddlewares.run(app.rootSaga);
 
+const LoadableApp = Loadable({
+  loader: () => import("@/pages/App/"),
+  loading: () => <Loading />,
+});
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LoadableApp} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root") as HTMLElement,
 );
---
+++ src/pages/Loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading: React.FunctionComponent = (props) => {
+  return <div>Loading...</div>;
+};
+
+export default Loading;
